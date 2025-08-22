4e58c6f9f7786b9bc445e8ffe7ca116bd3021a92
Kaushik Nagarajan <kaushik@tamu.edu>
2025-08-21 21:50:52 -0500

    documenting code

--- server.cpp
+++ server.cpp
@@ -8,12 +8,17 @@
 #include <cstring>
 #include <chrono>
 
+
+// Code for handling requests to the server
 void parseRequest(int client_socket){
+    // used to measure time
     auto start = std::chrono::high_resolution_clock::now();
+
+    // buffer is used to store the incoming request, 4096 characters is usually enough to store all requests
     char buffer[4096];
-    int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+    int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0); // receives the request from the user and stores it into the buffer
     if (bytes_received > 0) {
-        buffer[bytes_received] = '\0';
+        buffer[bytes_received] = '\0'; // ends the buffer with the null terminator
         std::cout << "\nRequest: " << buffer << std::endl;
 
 
@@ -24,7 +29,7 @@ void parseRequest(int client_socket){
             "\r\n"
             "<html><body><h1>Hello World!</h1></body></html>";
 
-        send(client_socket, response, strlen(response), 0);
+        send(client_socket, response, strlen(response), 0); // returns this response 
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
@@ -34,11 +39,13 @@ void parseRequest(int client_socket){
 }
 
 void setupSocket(int portNumber) {
+    // create the serverside (my socket)
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         std::cerr << "Failed to create socket." << std::endl;
         return;
     }
+    // setup stuff to set the port and ip addressd
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(portNumber);
@@ -57,8 +64,9 @@ void setupSocket(int portNumber) {
         return;
     }
     std::cout << "Server listening on port " << portNumber << "..." << std::endl;
-
+    // server is running
     while (true){
+        // accept requests
         int client_socket = accept(serverSocket, nullptr, nullptr);
         if (client_socket == -1){
             std::cerr << "Accept failed" << std::endl;
